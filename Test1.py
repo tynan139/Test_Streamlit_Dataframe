11df6ce2f09e99bf61347d7e97a7030828572a32
tynan139 <142603415+tynan139@users.noreply.github.com>
2023-09-08 15:52:12 +0100

    Update Test1.py
    dataframe with py not Py

--- Test1.py
+++ Test1.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.title('doh doht you work')
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+
+st.dataframe(df, use_container_width=True)
